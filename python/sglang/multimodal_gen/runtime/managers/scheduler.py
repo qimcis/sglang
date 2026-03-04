28ea6225556ae88a03af45f518e3888629c0a6fb
Chi McIsaac <chixie.mcisaac@gmail.com>
2026-03-04 12:52:19 -0500

    fix

--- python/sglang/multimodal_gen/runtime/managers/scheduler.py
+++ python/sglang/multimodal_gen/runtime/managers/scheduler.py
@@ -545,7 +545,7 @@ class Scheduler(SchedulerDisaggMixin):
                 output_file_paths=self._slice_batched_value(
                     output_batch.output_file_paths, start, end, total_items
                 ),
-                timings=deepcopy(output_batch.metrics),
+                metrics=deepcopy(output_batch.metrics),
                 noise_pred=self._slice_batched_value(
                     output_batch.noise_pred, start, end, total_items
                 ),
